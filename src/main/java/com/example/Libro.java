7188a031ed8c32a81f2945b2de11e9df7c2ff29f
sagumad <samuelmartinezguzman830@gmail.com>
2025-08-19 08:50:47 -0500

    cambioevaluacion

--- src/main/java/com/example/Libro.java
+++ src/main/java/com/example/Libro.java
@@ -2,23 +2,91 @@ package com.example;
 
 public class Libro {
     // TODO: Declarar atributos privados para titulo, autor y precio
-    
+    private String titulo;
+    private String autor;
+    private double precio;
+
     // TODO: Crear constructor con todos los atributos (titulo, autor, precio)
-    
+    public Libro(String titulo, String autor, double precio) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.precio = precio;
+    }
+
     // TODO: Crear constructor con solo titulo y autor (precio = 0.0)
-    
+    public Libro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.precio = 0.0;
+
+    }
+
     // TODO: Implementar métodos getter para cada atributo
-    
+    public String getTitulo() {
+        return titulo;
+
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public double getPrecio() {
+        return precio;
+
+    }
+
     // TODO: Implementar métodos setter para cada atributo
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
     // Nota: Validar que el precio no sea negativo
-    
+
+    public void setPrecio(double precio) {
+        if (precio < 0) {
+            System.out.println("el valor es invalido");
+        } else {
+            this.precio = precio;
+        }
+    }
+
     // TODO: Crear método aplicarDescuento que reciba porcentaje
+
+    public void aplicarDescuento(double porcentaje) {
+        if (porcentaje < 0 || porcentaje > 100) {
+            System.out.println("Porcentaje de descuento inválido.");
+            return;
+        }
+        double descuento = precio * (porcentaje / 100.0);
+        this.precio -= descuento;
+    }
     // Nota: Calcular descuento y actualizar precio
-    
-    // TODO: Crear método aplicarDescuento que reciba porcentaje y monto máximo
     // Nota: Aplicar descuento solo si no excede el monto máximo
-    
+    // TODO: Crear método aplicarDescuento que reciba porcentaje y monto máximo
+
+    public void aplicarDescuento(double porcentaje, double montoMaximo) {
+        if (porcentaje < 0 || porcentaje > 100) {
+            System.out.println("Porcentaje de descuento inválido.");
+            return;
+        }
+        double descuento = precio * (porcentaje / 100.0);
+        if (descuento > montoMaximo) {
+            System.out.println("Descuento excede el monto máximo permitido.");
+        } else {
+            this.precio -= descuento;
+        }
+    }
+
     // TODO: Crear método mostrarInformacion que imprima los datos del libro
-    
-    
+    public void mostrarInformacion() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Precio: " + precio);
+    }
+
 }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,16 +4,53 @@ public class Main {
     public static void main(String[] args) {
 
         // TODO: Crear método main para:
+
+
         // 1. Crear dos objetos Estudiante con diferentes valores
+        Estudiante estudiante1 = new Estudiante("miguel", 24, 4.2);
+        Estudiante estudiante2 = new Estudiante("angel", 21, 3.5);
+
+
         // 2. Mostrar la información de ambos estudiantes
+        estudiante1.mostrarInformacion();
+        estudiante2.mostrarInformacion();
+     
+
         // 3. Modificar algún atributo usando un setter
+        estudiante1.setEdad(25);
+        estudiante2.setPromedio(3.8);
+
+   
+
         // 4. Mostrar la información actualizada
+        estudiante1.mostrarInformacion();
+        estudiante2.mostrarInformacion();
+        
+        
 
         // TODO: Crear método main para:
+
         // 1. Crear un libro usando el constructor completo
+        Libro libro1 = new Libro("Dracula", "bram stocker", 250.000);
+            
+        
+
         // 2. Crear un libro usando el constructor con solo titulo y autor
+        Libro libro2 = new Libro("Cien Años de Soledad", "Gabriel García Márquez");
+        
+        
+        
         // 3. Mostrar información de ambos libros
+        libro1.mostrarInformacion();
+        libro2.mostrarInformacion();
         // 4. Aplicar descuento usando ambas versiones del método
+        libro1.aplicarDescuento(10); 
+        
+
+        libro2.aplicarDescuento(15.25); 
+        
         // 5. Mostrar información actualizada de los libros
+        libro1.mostrarInformacion();
+        libro2.mostrarInformacion();
     }
 }
